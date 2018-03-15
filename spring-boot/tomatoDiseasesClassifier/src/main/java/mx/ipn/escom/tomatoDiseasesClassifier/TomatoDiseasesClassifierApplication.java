1705d6a741dd76f091e3aa615924cf143839c2fe
earredondo <edgar.arredondo.basurto@gmail.com>
2018-03-14 23:00:47 -0600

    Spring boot application with classifier class

---
+++ spring-boot/tomatoDiseasesClassifier/src/main/java/mx/ipn/escom/tomatoDiseasesClassifier/TomatoDiseasesClassifierApplication.java
@@ -0,0 +1,12 @@
+package mx.ipn.escom.tomatoDiseasesClassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoDiseasesClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoDiseasesClassifierApplication.class, args);
+	}
+}
